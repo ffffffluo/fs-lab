077d8ba7bf78fa65f2c6e97f9e49fdff362c1bea
Fan Luo <fluotechdev@gmail.com>
2026-01-19 00:02:43 -0600

    Refactor Header component to use imported logo instead of relative path

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
+import logo from "../assets/logo.png";
+
 function Header() {
   return (
     <header>
       <div className="header-content">
-        <img
-          src="../assets/logo.png"
-          alt="Pixell River Logo"
-          className="logo"
-        />
+        <img src={logo} alt="Pixell River Logo" className="logo" />
         <div className="title-group">
           <h1>Pixell River Employee Directory</h1>
           <p>Manage and view our organizational structure.</p>
