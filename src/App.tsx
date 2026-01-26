56a8cbd7d8f2900477a1b8a70915487a3c629d0a
Fan Luo <fluotechdev@gmail.com>
2026-01-25 23:33:02 -0600

    Add EmployeeForm component and integrate employee addition functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Department from "./components/Department";
-import { organizationData } from "./data/organizationData";
+import EmployeeForm from "./components/EmployeeForm";
+import { organizationData as initialData } from "./data/organizationData";
 
 function App() {
+  const [data, setData] = useState(initialData);
+
+  const addEmployee = (
+    firstName: string,
+    lastName: string,
+    deptName: string,
+  ) => {
+    const updatedData = data.map((dept) => {
+      if (dept.name === deptName) {
+        return {
+          ...dept,
+          employees: [...dept.employees, { firstName, lastName }],
+        };
+      }
+      return dept;
+    });
+    setData(updatedData);
+  };
+
   return (
     <div>
       <Header />
       <main>
-        {organizationData.map((dept, index) => (
+        {data.map((dept, index) => (
           <Department key={index} department={dept} />
         ))}
+        <EmployeeForm
+          departments={data.map((d) => d.name)}
+          onAdd={addEmployee}
+        />
       </main>
       <Footer />
     </div>
