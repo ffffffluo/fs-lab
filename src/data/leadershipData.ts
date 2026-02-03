56bf60cef513bf95cc88b6b110ef610094a526a2
Fan Luo <fluotechdev@gmail.com>
2026-02-02 18:26:31 -0600

    feat: add leadership data and update Officer interface in types

---
+++ src/data/leadershipData.ts
@@ -0,0 +1,9 @@
+import type { Officer } from "../types/types";
+
+export const leadershipData: Officer[] = [
+  { firstName: "Jo-Anne", lastName: "Sinclair", role: "CEO & Chair of Board" },
+  { firstName: "Jackson", lastName: "Smith", role: "COO & VP Operations" },
+  { firstName: "Susan", lastName: "Thomas", role: "CFO & VP Administration" },
+  { firstName: "Richa", lastName: "Kaur", role: "VP Client Services" },
+  { firstName: "Josee", lastName: "Benjamin", role: "CIO" },
+];
